fd0258a1195f1c4faf71a071e656e3b95f283c72
ericklarsen <ericklarsenn@gmail.com>
2022-09-20 00:07:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add Express
+const express = require('express');
+
+// Initialize Express
+const app = express();
+
+// Create GET Request
+app.get('/', (req, res) => {
+    res.send('Express on vercel');
+});
+
+// Initialize Server
+app.listen(5000, () => {
+    console.log('Running on port 5000');
+});
+
+// Export the Express API
+module.exports = app;
